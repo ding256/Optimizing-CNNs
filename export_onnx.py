56a9dc5bc216ba325aea4a56ed2c12e38657b4b9
ding256 <131704237+ding256@users.noreply.github.com>
2025-05-16 01:25:45 -0400

    Update export_onnx.py

--- export_onnx.py
+++ export_onnx.py
@@ -9,10 +9,7 @@ os.makedirs("models", exist_ok=True)
 with torch.serialization.safe_globals([models.resnet.ResNet]):
     quantized_model = torch.load("models/resnet18_pruned_quantized_full.pth", weights_only=False)
 
-# Confirm the model is in evaluation mode
-quantized_model.eval()
-
-# Export the Quantized Model to ONNX
+# Directly Export the Model without `.eval()`
 dummy_input = torch.randn(1, 3, 32, 32)
 onnx_path = "models/resnet18_int8.onnx"
 torch.onnx.export(quantized_model, dummy_input, onnx_path, opset_version=13)
