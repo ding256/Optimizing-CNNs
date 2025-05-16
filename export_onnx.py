d91dcaf5eab3a70ecca6e7d92b9f3cdc824f8f89
ding256 <131704237+ding256@users.noreply.github.com>
2025-05-16 00:44:44 -0400

    Update export_onnx.py

--- export_onnx.py
+++ export_onnx.py
@@ -1,15 +1,18 @@
 import os
 import torch
+from torchvision import models
 
 # Ensure directories exist
 os.makedirs("models", exist_ok=True)
 
-# Load the Full Quantized Model (weights_only=False)
-quantized_model = torch.load("models/resnet18_pruned_quantized_full.pth", weights_only=False)
-quantized_model.eval()
+# Load the Quantized Model (State Dict)
+model = models.resnet18()
+model.fc = torch.nn.Linear(model.fc.in_features, 10)
+model.load_state_dict(torch.load("models/resnet18_pruned_quantized_state.pth"))
+model.eval()
 
 # Export to ONNX
 dummy_input = torch.randn(1, 3, 32, 32)
 onnx_path = "models/resnet18_int8.onnx"
-torch.onnx.export(quantized_model, dummy_input, onnx_path, opset_version=13)
+torch.onnx.export(model, dummy_input, onnx_path, opset_version=13)
 print(f"ONNX model saved to {onnx_path}")
